2221df9635e1443da14d7ed2e4d8f99bccc0da8c
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2023-09-30 11:19:53 +0530

    React-Started

---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () =>{
+    return (
+        <div>
+            <h1>Hello World from Login</h1>
+        </div>
+    );
+};
+
+export default Login;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/js/dist/dropdown";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/about">
+                About
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/contact">
+                Contact
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/login">
+                Login
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/signup">
+                Registration
+              </a>
+            </li>
+
+
+          </ul>
+          
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
